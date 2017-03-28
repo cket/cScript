990946d09d1c6a69a53c2b7380928f9babe05423
CJ Ketchum <cketchum@ucsc.edu>
2017-03-27 22:10:53 -0700

    Implement stack operations

--- compile.py
+++ compile.py
@@ -9,14 +9,46 @@ class SyntaxError(RuntimeError):
 def printFn():
 	print stack.pop()
 
+
 def plusFn():
 	stack.append(stack.pop() + stack.pop())
+
+
+def dupFn():
+	stack.append(stack[-1])
+
+
+def dropFn():
+	stack.pop()
+
+
+def swapFn():
+	tos = stack.pop()
+	stack.insert(-2, tos)
+
+
+def overFn():
+	stack.append(stack[-2])
+
+
+def rotateFn():
+	tos = stack.pop()
+	second = stack.pop()
+	stack.insert(-2, second)
+	stack.insert(-2, tos)
 
 def populateKeyWords():
 	Print = {"print": printFn}
 	Math = {"+": plusFn}
+	Stack = {"dup": dupFn, # copy TOS
+			 "drop": dropFn, # pop
+			 "swap": swapFn, # swap TOS and 2OS
+			 "over": overFn, # Copy 2OS to TOS
+			 "rotate": rotateFn, # Move 3OS to TOS
+			 }
 	keywords.update(Print)
 	keywords.update(Math)
+	keywords.update(Stack)
 
 def parseTokens():
 	code_file = sys.argv[1]
