ca29cd00ca54dd5e1214b1ae23f8a088e25b64c1
CJ Ketchum <cketchum@ucsc.edu>
2017-03-27 21:31:39 -0700

    Parser skeleton

--- compile.py
+++ compile.py
@@ -1,3 +1,27 @@
 import sys
 
 code_file = sys.argv[1]
+
+if not code_file.endswith(".ck"):
+	raise SyntaxError("File extension must be '.ck'")
+
+with open(code_file) as f:
+	tokens = f.read()
+
+tokens = tokens.split()
+
+keywords = {}
+stack = []
+
+for token in tokens:
+	if token in keywords:
+		pass
+	try:
+		token = float(token)
+		stack.append(token)
+	except ValueError:
+		raise SyntaxError(token)
+
+
+class SyntaxError(RuntimeError):
+	pass
