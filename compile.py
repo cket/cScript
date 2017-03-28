6601c312cde5fa5f6344632a75a3eb3ae61e5bfb
CJ Ketchum <cketchum@ucsc.edu>
2017-03-27 21:49:34 -0700

    Implement print & adding functionality

--- compile.py
+++ compile.py
@@ -1,27 +1,44 @@
 import sys
 
-code_file = sys.argv[1]
-
-if not code_file.endswith(".ck"):
-	raise SyntaxError("File extension must be '.ck'")
-
-with open(code_file) as f:
-	tokens = f.read()
-
-tokens = tokens.split()
-
 keywords = {}
 stack = []
 
-for token in tokens:
-	if token in keywords:
-		pass
-	try:
-		token = float(token)
-		stack.append(token)
-	except ValueError:
-		raise SyntaxError(token)
-
-
 class SyntaxError(RuntimeError):
 	pass
+
+def printFn():
+	print stack.pop()
+
+def plusFn():
+	stack.append(stack.pop() + stack.pop())
+
+def populateKeyWords():
+	Print = {"print": printFn}
+	Math = {"+": plusFn}
+	keywords.update(Print)
+	keywords.update(Math)
+
+def parseTokens():
+	code_file = sys.argv[1]
+
+	if not code_file.endswith(".ck"):
+		raise SyntaxError("File extension must be '.ck'")
+
+	with open(code_file) as f:
+		tokens = f.read()
+	return tokens.split()
+
+if __name__ == "__main__":
+	populateKeyWords()
+	tokens = parseTokens()
+
+	for token in tokens:
+		if token in keywords:
+			keywords[token]()
+		else:
+			try:
+				token = float(token)
+				stack.append(token)
+			except ValueError:
+				raise SyntaxError(token)
+
