87df6bfc9593257f3999544d3dffab69dd63d1a2
CJ Ketchum <cketchum@ucsc.edu>
2017-03-31 14:52:18 -0700

    Encapsulate into Compile object

--- compile.py
+++ compile.py
@@ -1,76 +1,89 @@
 import sys
 
-keywords = {}
-stack = []
 
 class SyntaxError(RuntimeError):
 	pass
 
-def printFn():
-	print stack.pop()
+class Compiler(object):
+	def __init__(self, code_file):
+		self.keywords = self.populateKeyWords()
+		self.tokens = self.parseTokens(code_file)
+		self.stack = []
+
+	def run(self):
+		for token in self.tokens:
+			if token in self.keywords:
+				self.keywords[token]()
+			else:
+				try:
+					token = float(token)
+					self.stack.append(token)
+				except ValueError:
+					raise SyntaxError(token)
+
+	def synWrapper(self, m):
+		try:
+			m()
+		except:
+			raise SyntaxError("Problem occured during execution of %s" % m.__name__)
 
 
-def plusFn():
-	stack.append(stack.pop() + stack.pop())
+	def printFn(self):
+		print self.stack.pop()
 
 
-def dupFn():
-	stack.append(stack[-1])
+	def plusFn(self):
+		self.stack.append(self.stack.pop() + self.stack.pop())
+
+	def dupFn(self):
+		self.stack.append(self.stack[-1])
+
+	def dropFn(self):
+		self.stack.pop()
+
+	def swapFn(self):
+		tos = self.stack.pop()
+		self.stack.insert(-2, tos)
+
+	def overFn(self):
+		self.stack.append(stack[-2])
 
 
-def dropFn():
-	stack.pop()
+	def rotateFn(self):
+		tos = self.stack.pop()
+		second = self.stack.pop()
+		self.stack.insert(-2, second)
+		self.stack.insert(-2, tos)
 
 
-def swapFn():
-	tos = stack.pop()
-	stack.insert(-2, tos)
+	def populateKeyWords(self):
+		keywords = {}
+		Print = {"print": self.printFn}
+		Math = {"+": self.plusFn}
+		Stack = {"dup": self.dupFn, # copy TOS
+				 "drop": self.dropFn, # pop
+				 "swap": self.swapFn, # swap TOS and 2OS
+				 "over": self.overFn, # Copy 2OS to TOS
+				 "rotate": self.rotateFn, # Move 3OS to TOS
+				 }
+		keywords.update(Print)
+		keywords.update(Math)
+		keywords.update(Stack)
+		return keywords
+
+	def parseTokens(self, code_file):
+		if not code_file.endswith(".ck"):
+			raise SyntaxError("File extension must be '.ck'")
+
+		with open(code_file) as f:
+			tokens = f.read()
+		return tokens.split()
 
 
-def overFn():
-	stack.append(stack[-2])
-
-
-def rotateFn():
-	tos = stack.pop()
-	second = stack.pop()
-	stack.insert(-2, second)
-	stack.insert(-2, tos)
-
-def populateKeyWords():
-	Print = {"print": printFn}
-	Math = {"+": plusFn}
-	Stack = {"dup": dupFn, # copy TOS
-			 "drop": dropFn, # pop
-			 "swap": swapFn, # swap TOS and 2OS
-			 "over": overFn, # Copy 2OS to TOS
-			 "rotate": rotateFn, # Move 3OS to TOS
-			 }
-	keywords.update(Print)
-	keywords.update(Math)
-	keywords.update(Stack)
-
-def parseTokens():
-	code_file = sys.argv[1]
-
-	if not code_file.endswith(".ck"):
-		raise SyntaxError("File extension must be '.ck'")
-
-	with open(code_file) as f:
-		tokens = f.read()
-	return tokens.split()
 
 if __name__ == "__main__":
-	populateKeyWords()
-	tokens = parseTokens()
+	c = Compiler(sys.argv[1])
+	c.run()
 
-	for token in tokens:
-		if token in keywords:
-			keywords[token]()
-		else:
-			try:
-				token = float(token)
-				stack.append(token)
-			except ValueError:
-				raise SyntaxError(token)
+
 
